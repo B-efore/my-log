a97820d45dfc40da9c8ccadf9742c36784ee70d7
jiwon <ljw010512@naver.com>
2025-03-21 21:49:28 +0900

    feat: BaseEnum 및 Enum 클래스 추가

--- src/main/java/com/jiwon/mylog/entity/Visibility.java
+++ src/main/java/com/jiwon/mylog/entity/Visibility.java
@@ -1,5 +1,25 @@
 package com.jiwon.mylog.entity;
 
-public enum Visibility {
-    PUBLIC, PRIVATE;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.jiwon.mylog.entity.base.BaseEnum;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum Visibility implements BaseEnum<Visibility> {
+    PUBLIC("공개"),
+    PRIVATE("비공개");
+
+    private final String status;
+
+    @Override
+    @JsonValue
+    public String getStatus() {
+        return status;
+    }
+
+    @JsonCreator
+    public static Visibility fromString(String name) {
+        return BaseEnum.fromString(Visibility.class, name);
+    }
 }
--- src/main/java/com/jiwon/mylog/entity/post/PostStatus.java
+++ src/main/java/com/jiwon/mylog/entity/post/PostStatus.java
@@ -1,8 +1,27 @@
 package com.jiwon.mylog.entity.post;
 
-public enum PostStatus {
-    DRAFT,
-    PUBLISHED,
-    HIDDEN,
-    DELETED;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.jiwon.mylog.entity.base.BaseEnum;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum PostStatus implements BaseEnum<PostStatus> {
+    DRAFT("임시 저장됨"),
+    PUBLISHED("게시됨"),
+    HIDDEN("숨김 처리됨"),
+    DELETED("삭제됨");
+
+    private final String status;
+
+    @Override
+    @JsonValue
+    public String getStatus() {
+        return status;
+    }
+
+    @JsonCreator
+    public static PostStatus fromString(String name) {
+        return BaseEnum.fromString(PostStatus.class, name);
+    }
 }
