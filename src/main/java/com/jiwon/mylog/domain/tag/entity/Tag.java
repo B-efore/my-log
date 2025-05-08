b6d9d284edfaa96b55c0c74b4b581d0a22c4e6ab
jiwon <ljw010512@naver.com>
2025-05-08 20:37:42 +0900

    fix: 태그 user 및 name으로 복합 유니크 설정하도록 변경

--- src/main/java/com/jiwon/mylog/domain/tag/entity/Tag.java
+++ src/main/java/com/jiwon/mylog/domain/tag/entity/Tag.java
@@ -11,6 +11,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -24,12 +26,18 @@ import java.util.List;
 @Builder
 @Getter
 @Entity
+@Table(
+        name = "tag",
+        uniqueConstraints = @UniqueConstraint(
+                name = "tag_uk",
+                columnNames = {"user_id", "name"}
+        )
+)
 public class Tag {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true)
     private String name;
 
     @ManyToOne(fetch = FetchType.LAZY)
