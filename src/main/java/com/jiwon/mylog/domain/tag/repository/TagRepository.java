1fc756db28fdf1c1ffc149614bed8ad96a1c236b
jiwon <ljw010512@naver.com>
2025-05-08 21:10:02 +0900

    refactor: Tag 조회 및 생성 시 조회 쿼리 한 번만 나가도록  개선

--- src/main/java/com/jiwon/mylog/domain/tag/repository/TagRepository.java
+++ src/main/java/com/jiwon/mylog/domain/tag/repository/TagRepository.java
@@ -2,6 +2,7 @@ package com.jiwon.mylog.domain.tag.repository;
 
 import com.jiwon.mylog.domain.tag.entity.Tag;
 import com.jiwon.mylog.domain.user.entity.User;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,6 +11,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
-    @Query(value = "select t from Tag t where t.user = :user and t.name = :name")
-    Optional<Tag> findTagByUserAndName(@Param("user") User user, @Param("name") String name);
+    List<Tag> findAllByUserAndNameIn(User user, List<String> names);
 }
