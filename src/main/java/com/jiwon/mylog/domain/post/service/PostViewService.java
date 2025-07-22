55c2516b375706840b02717fe16aa95c46a573c7
B-efore <ljw010512@naver.com>
2025-07-22 10:15:10 +0900

    refactor: Redis 조회수 TTL 관리를 위해 hash -> value 변경

--- src/main/java/com/jiwon/mylog/domain/post/service/PostViewService.java
+++ src/main/java/com/jiwon/mylog/domain/post/service/PostViewService.java
@@ -11,22 +11,23 @@ public class PostViewService {
 
     private final RedisUtil redisUtil;
     private static final String VIEW_KEY_PREFIX = "post:view:";
-    private static final String VIEW_COUNT_KEY_PREFIX = "post:view:count";
+    private static final String VIEW_COUNT_KEY_PREFIX = "post:view:count:";
 
     public int incrementPostView(Long postId, int view, String userKey) {
         String existKey = VIEW_KEY_PREFIX + postId;
-        String countKey = VIEW_COUNT_KEY_PREFIX;
+        String countKey = VIEW_COUNT_KEY_PREFIX + postId;
 
         boolean exist = redisUtil.existPostViewUser(existKey, userKey);
         if (!exist) {
             redisUtil.addPostViewUser(existKey, userKey);
-            redisUtil.increasePostView(countKey, postId.toString(), String.valueOf(view));
+            redisUtil.increasePostView(countKey, String.valueOf(view));
         }
 
         return getPostView(postId, view);
     }
 
     public int getPostView(Long postId, int view) {
-        return redisUtil.getPostView(VIEW_COUNT_KEY_PREFIX, postId.toString(), view);
+        String key = VIEW_COUNT_KEY_PREFIX + postId;
+        return redisUtil.getPostView(key, view);
     }
 }
