39fafd33a6eb32d5d4bdd2ecb085fe9572d1afff
B-efore <ljw010512@naver.com>
2025-07-25 23:59:58 +0900

    fix: openai 프롬프트 메세지 수정

--- src/main/java/com/jiwon/mylog/domain/gpt/OpenAiService.java
+++ src/main/java/com/jiwon/mylog/domain/gpt/OpenAiService.java
@@ -48,14 +48,14 @@ public class OpenAiService {
 
     private ChatResponse callOpenAiApi(Long userId) {
         SystemMessage systemMessage = new SystemMessage("""
-                    넌 오늘의 운세를 나폴리탄 괴담 형식으로 알려주는 예언자야.
-                    기묘한 단어를 조금 섞어서 무서운 괴담 느낌 가득하게 운세를 말해줘.
-                    추가로 운세에는 아래 내용을 짧게 포함해야 해.
-                    - 행운의 색, 행운의 물건, 나폴리탄 형식 괴담 주의사항 하나
+                    넌 범접할 수 없는 영적인 존재야. 오늘의 운세를 전하면서 일상에 스며든 괴담처럼 말해줘.
+                    - 기묘하거나 낯선 단어를 섞어 공포 분위기를 조성해
+                    - 운세라는 말을 꺼내지 말고 마치 이야기를 툭 꺼내듯 시작하고 말투는 담담하게 진행해
+                    - 운세에는 (행운의 색, 행운의 물건, 나폴리탄 괴담 형식의 주의사항 하나)를 포함해
                 """);
         String date = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일"));
         UserMessage userMessage = new UserMessage(
-                String.format("사용자 %d, %s 기준으로 오늘의 외계인 운세 알려줘.", userId, date));
+                String.format("사용자 %d, %s 기준으로 오늘의 괴담 운세 알려줘.", userId, date));
 
         List<Message> messages = Arrays.asList(systemMessage, userMessage);
 
